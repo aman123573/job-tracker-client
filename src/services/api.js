3c10bc3cada54c715570b17a0e1b6f6519500afb
Aman Kumar <akmu7835@gmail.com>
2026-06-10 16:40:33 +0530

    update api base url for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    // baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://job-tracker-api-production-b894.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
